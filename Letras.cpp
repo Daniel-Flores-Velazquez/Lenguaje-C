bb4f3ce74b72cd4bf4ae3c87e11fb01bb7f45ad9
Daniel-Flores-Velazquez <120158137+Daniel-Flores-Velazquez@users.noreply.github.com>
2023-04-26 16:43:18 -0600

    Add files via upload

--- Letras.cpp
+++ Letras.cpp
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
-	char letra;
-	printf("Programa para ver si es vocal o no\n");
-	while(letra =='\0'){
-		printf("\nIngrese una letra: ");	
-		scanf("%s",&letra);
-		if((letra=='a')||(letra=='e')||(letra=='i')||(letra=='o')||(letra=='u')){
-			printf("\nEs una vocal");
-		}else{
-			printf("\nNo es una vocal");
-		}
+	char letras[]={'a','b','c','d','e','f','g','h','i','j','k','l','m'
+					,'n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	int i;
+	printf("Programa para mostrar las letras del abecedario\n");
+	for(i=0; i<26;i++){
+			printf("%c, ",letras[i]);
 	}
+	/*No funciona por que el tamaño del arreglo no es el mismo que el total
+	  de las letras*/
 }
--- Pares.cpp
+++ Pares.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-    int numero;
-    printf("Programa para imprimir pares del 1 al 100\n");
-    for ( numero = 2 ; numero <= 100 ; numero += 2 ){
-        printf( "%d, ", numero );
-    }
+int main(){
+	int N, pares[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
+					,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42};
+	printf("Programa para mostrar numeros pares en un arreglo\n");
+	for(N=0; N<40;N++){
+		if(pares[N]%2 ==0){
+			printf("%d\n",pares[N]);
+		}
+	}
 }
